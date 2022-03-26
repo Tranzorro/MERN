1602fcc33bce6ac3413fc8e0ee2d74473a702772
Tranzorro <shiekadam@gmail.com>
2022-03-26 16:11:39 -0400

    Update product.model.js
    error messages now function. freakin finally.

--- ProductManager1/server/models/product.model.js
+++ ProductManager1/server/models/product.model.js
@@ -6,18 +6,18 @@ const ProductSchema = new mongoose.Schema({
     Title: {
         type: String,
         message: "needs a name, duh",
-        minlength: 4,
+        minlength: [4, "make it more than 4 characters please."],
         required: [true,"needs a name, duh"],
     },
     Price: {
         type: Number,
         required: [true,"how expensive is it?"],
-        minlength: 1,
+        minlength: [1, "needs to be at least not blank my dude."],
     },
     Description: {
         type: String,
         required: [true,"what is it?"],
-        minlength: 10,
+        minlength: [10,"use your words..."],
     },
 }, {timestamps: true});
 
